39654cf0304948f2246e3c96d421bea8cd1e586f
eotsuki <e.otsuki@bizbase.co.jp>
2020-09-23 17:44:42 +0900

    sample

---
+++ src/app/user/user-edit/user-edit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+  user: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: UserService
+  ) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.user = this.service.getUser(id);
+  }
+
+  onSubmit(form: any): void {
+    let user = {
+      id: form.id,
+      name: form.name,
+      email: form.email
+    };
+    this.service.setUser(user);
+    this.router.navigate(['/users']);
+  }
+}
